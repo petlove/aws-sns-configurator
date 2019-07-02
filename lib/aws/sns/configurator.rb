b21c1e72a6d19c38dae32db021686ce6fecdd89e
linqueta <lincolnrodrs@gmail.com>
2019-07-01 23:14:52 -0300

    set travis

--- lib/aws/sns/configurator.rb
+++ lib/aws/sns/configurator.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require 'aws/sns/configurator/version'
-require 'aws/sns/configurator/topic'
-require 'aws/sns/configurator/reader'
 require 'aws-sdk-sns'
 
 module AWS
